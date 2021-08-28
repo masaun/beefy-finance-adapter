fb1d8cdc3456379889da4b5ee4290f90ae5564c5
masaun <masanoriuno@gmail.com>
2021-08-28 20:00:04 +0900

    fix: add IERC20V2.sol that IERC20.sol is inherited

---
+++ contracts/beefy.finance/openzeppelin-solidity/IERC20V2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+import { IERC20 } from "./v2.5.1/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
+ * the optional functions; to access them see {ERC20Detailed}.
+ */
+contract IERIERC20V2 is IERC20 {
+
+}
