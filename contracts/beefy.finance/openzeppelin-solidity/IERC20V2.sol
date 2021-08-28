4084e3edf6343084d5888de4dc8fbd15fa7f07a2
masaun <masanoriuno@gmail.com>
2021-08-28 21:29:41 +0900

    fix: add explanation to IERC20V2.sol

--- contracts/beefy.finance/openzeppelin-solidity/IERC20V2.sol
+++ contracts/beefy.finance/openzeppelin-solidity/IERC20V2.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.5.0;
 import { IERC20 } from "./v2.5.1/contracts/token/ERC20/IERC20.sol";
 
 /**
- * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
- * the optional functions; to access them see {ERC20Detailed}.
+ * @notice - IERC20V2.sol is the contract that IERC20.sol is inherited in order to avoid duplicate contract name with IERC20.sol of openzeppelin/contracts v3.4.0
  */
 contract IERC20V2 is IERC20 {
 
